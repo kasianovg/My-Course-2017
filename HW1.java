9cf52a307175ef1c74c08b0f267360b5aefd624e
kasianovg <montoya.xxm@gmail.com>
2017-09-09 21:22:37 +0300

    HW1 extension corrected

---
+++ HW1.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The June of 2017 summer I will do absolutely nothing productive , July - the same," +
+                "but the August - will be a bit special, so I decide to learn Java to integrate it in my baggage of " +
+                "knowledge and skills.  ");
+        }
+    }
+
+
